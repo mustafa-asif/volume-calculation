5cef3505f02682aa8a383167b52c8a458d43a87a
USER <USER@DESKTOP-3612A8B>
2023-03-01 22:08:03 +0500

    Add project files.

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,26 @@
+// ConsoleApplication1.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int lenght;
+	int height;
+	int widht;
+	int volume;
+	cin >> lenght >> height >> widht;
+	volume = lenght * height * widht;
+	cout << volume << endl;
+	system("pause");
+	return 0;
+
+}
+
+
+
+
+
+
